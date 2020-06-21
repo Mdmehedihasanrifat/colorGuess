83ab6ebd90ee315afcb5fb1257e22f3c93b83d87
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2020-06-21 17:37:44 +0600

    random color

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-let color=[
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 255, 255)",
-  "rgb(255, 0, 255)"
-]
+let color=genraterandomColor(6);
 
-let colorpick=color[3];
+let colorpick=colorPick();
+
+let h1=document.querySelector('h2');
+
+
 
 let message=document.getElementById('message');
 let displayRgB=document.getElementById("displayRgb");
@@ -20,9 +17,12 @@ for(let i=0 ;i< squares.length;i++){
 squares[i].addEventListener("click",function () {
 
   let clickColor=this.style.backgroundColor;
-
+ console.log(clickColor);
+ console.log(colorpick);
   if(clickColor===colorpick){
     message.innerText="correct";
+    colorChange(clickColor);
+    h1.style.backgroundColor=clickColor;
   }
   else{
 this.style.backgroundColor="#232323";
@@ -33,4 +33,49 @@ message.innerText="Try again";
 
 }
 
-alert("hello");
+function colorChange(color) {
+
+  for(let i=0 ;i< squares.length;i++){
+
+    squares[i].style.backgroundColor=color;
+
+
+   colorPick();
+
+
+  }
+
+}
+
+
+function colorPick() {
+
+  let random=Math.round(Math.random()*5);
+  console.log(random);
+  return color[random];
+}
+
+
+function genraterandomColor(num) {
+
+  let arr=[];
+
+  for(let i=0 ;i<num;i++) {
+
+    arr.push(randomColor());
+
+  }
+
+    return arr;
+
+}
+function randomColor() {
+
+let r,g,b;
+  r=Math.round(Math.random()*255);
+
+  g=Math.round(Math.random()*255);
+  b=Math.round(Math.random()*255);
+
+  return "rgb("+r+", "+g+", "+b+")";
+}
